9cb664d76d077b43270c5a1daa5c3be8b19418d8
Renzo Damian <renzo.damian.go@gmail.com>
2022-08-04 17:29:34 -0500

    Prettier format all project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    colors: {
-      white: "#ffffff",
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		colors: {
+			white: '#ffffff',
+			blue: '#1fb6ff',
+			purple: '#7e5bef',
+			pink: '#ff49db',
+			orange: '#ff7849',
+			green: '#13ce66',
+			yellow: '#ffc82c',
+			'gray-dark': '#273444',
+			gray: '#8492a6',
+			'gray-light': '#d3dce6',
+		},
+		extend: {},
+	},
+	plugins: [],
 };
